269f7de6270a469e108897fc582202a8f2f668ee
Santiago <santiagow@126.com>
2016-12-22 00:13:45 +0800

    #1 package and submit to spark
     - use config file
     - update build.gradle

--- src/main/scala/com/santiago/spark/App.scala
+++ src/main/scala/com/santiago/spark/App.scala
@@ -1,9 +1,15 @@
 package com.santiago.spark
 
+import java.io.{File, FileInputStream}
+import java.security.InvalidParameterException
 import java.util.regex.Pattern
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.Constructor
+
+import scala.beans.BeanProperty
 
 object App {
   val conf = new SparkConf().setAppName("spark_demo").setMaster("local")
@@ -11,18 +17,35 @@ object App {
 
   val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
   val sqlContext: SQLContext = spark.sqlContext
+  val baseDir: String = System.getProperty("user.dir")
+  var appArgs: AppArgs = _
+  val outputDir: String = baseDir + File.separator + "output"
 
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "E:\\logs")
+    if (args.length == 0) {
+      throw new InvalidParameterException("please specify a config file")
+    }
+
+    val confPath = args(0)
+    loadConfig(confPath)
+
+    if (System.getProperty("hadoop.home.dir") == null) {
+      System.setProperty("hadoop.home.dir", outputDir)
+    }
 
     //wordCount()
     //piEstimat()
     sql()
   }
 
+  def loadConfig(confPath: String): Unit = {
+    val yaml = new Yaml(new Constructor(classOf[AppArgs]))
+    appArgs = yaml.load(new FileInputStream(confPath)).asInstanceOf[AppArgs]
+  }
+
   def wordCount(): Unit = {
-    val logFile = "E:\\project_reference\\SparkDemo\\build.gradle"
-    val textFile = sc.textFile(logFile)
+    val classloader:ClassLoader = Thread.currentThread().getContextClassLoader()
+    val textFile = sc.textFile(classloader.getResource("concurrency_utilities_overview.md").getPath)
     val regEx = "^[_a-zA-Z]\\w*"
     val pattern = Pattern.compile(regEx)
     val counts = textFile.flatMap(line => line.split("\\W"))
@@ -37,7 +60,8 @@ object App {
     counts.collect().foreach(a => {
       println(a._1 + ": " + a._2)
     })
-    //counts.saveAsTextFile("E:\\project_reference\\SparkDemo\\output")
+
+    //counts.saveAsTextFile(outputDir)
   }
 
   def piEstimat(): Unit = {
@@ -51,12 +75,12 @@ object App {
   }
 
   def sql(): Unit = {
-    val url = "jdbc:mysql://localhost:3306/sampdb?user=test&password=test"
+    val url = getJdbcUrl()
     val df = sqlContext
       .read
       .format("jdbc")
       .option("url", url)
-      .option("driver", "com.mysql.jdbc.Driver")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
       .option("dbtable", "member")
       .load()
 
@@ -64,11 +88,35 @@ object App {
     df.printSchema()
 
     // Counts people by age
-    val countsByAge = df.groupBy("state").count()
-    countsByAge.show()
+    val countsByState = df.groupBy("state").count()
+    countsByState.show()
 
     // Saves countsByAge to S3 in the JSON format.
-    //val dateFrame = countsByAge.write.format("json").save("s3a://...")
+//    val dataFrame = countsByState.write.format("json").save(outputDir + File.separator + "count_by_state.json")
+//    println(dataFrame)
   }
 
+  def getJdbcUrl() : String = {
+    "jdbc:mysql://" + appArgs.db.host + ":" + appArgs.db.port + "/" +
+      appArgs.db.dbName + "?user=" + appArgs.db.user + "&password=" + appArgs.db.password
+  }
+
+}
+
+class AppArgs {
+  @BeanProperty
+  var db: DbArgs = _
+}
+
+class DbArgs {
+  @BeanProperty
+  var host:String = _
+  @BeanProperty
+  var port = 3306
+  @BeanProperty
+  var dbName:String = _
+  @BeanProperty
+  var user:String = _
+  @BeanProperty
+  var password:String = _
 }
